d944fe78f5c55d1032373fbabaf7f105fffe7282
PP6464 <panthhp@outlook.com>
2024-07-19 12:39:01 +0100

    refresh password list

--- composeApp/src/commonMain/kotlin/pages/Home.kt
+++ composeApp/src/commonMain/kotlin/pages/Home.kt
@@ -1,16 +1,18 @@
 package pages
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -36,13 +38,15 @@ import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.painterResource
 import passmanager.composeapp.generated.resources.Res
 import passmanager.composeapp.generated.resources.ic_add
+import passmanager.composeapp.generated.resources.ic_refresh
 import passmanager.composeapp.generated.resources.mont
+import java.lang.Thread.sleep
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomePage(navigator: Navigator) {
 	val montserrat = FontFamily(Font(Res.font.mont))
 	var passwords by remember { mutableStateOf<List<Password>>(emptyList()) }
+	var refreshLoading by remember { mutableStateOf(false) }
 	
 	suspend fun refresh() {
 		Appwrite.fetchPasswords { res ->
@@ -58,15 +62,43 @@ fun HomePage(navigator: Navigator) {
 	
 	Scaffold(
 		floatingActionButton = {
-			Button(
-				onClick = {
-					println("HI")
-				}
+			Row(
+				verticalAlignment = Alignment.CenterVertically
 			) {
-				Icon(
-					painter = painterResource(Res.drawable.ic_add),
-					contentDescription = null
-				)
+				if (!refreshLoading) {
+					Button(
+						onClick = {
+							refreshLoading = true
+							CoroutineScope(Dispatchers.IO).launch {
+								refresh()
+								refreshLoading = false
+							}
+						}
+					) {
+						Icon(
+							painter = painterResource(Res.drawable.ic_refresh),
+							contentDescription = null,
+						)
+					}
+				} else {
+					CircularProgressIndicator(
+						strokeWidth = 2.dp,
+						modifier = Modifier
+							.width(32.dp)
+							.height(32.dp)
+					)
+				}
+				Box(modifier = Modifier.width((if (refreshLoading) 16 else 8).dp))
+				Button(
+					onClick = {
+						println("HI")
+					}
+				) {
+					Icon(
+						painter = painterResource(Res.drawable.ic_add),
+						contentDescription = null
+					)
+				}
 			}
 		}
 	) {
