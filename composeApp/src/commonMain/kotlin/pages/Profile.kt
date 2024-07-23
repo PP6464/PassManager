d72d49d81038eb4b9c33ea622d0e756114cf6af6
PP6464 <panthhp@outlook.com>
2024-07-23 21:08:56 +0100

    profile page

--- composeApp/src/commonMain/kotlin/pages/Profile.kt
+++ composeApp/src/commonMain/kotlin/pages/Profile.kt
@@ -1,9 +1,11 @@
 package pages
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,19 +13,28 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import backend.Appwrite
 import moe.tlaster.precompose.navigation.Navigator
+import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.painterResource
 import passmanager.composeapp.generated.resources.Res
 import passmanager.composeapp.generated.resources.ic_back
+import passmanager.composeapp.generated.resources.ic_email
+import passmanager.composeapp.generated.resources.mont
 
 @Composable
 fun ProfilePage(navigator: Navigator) {
-	var email: String? by remember { mutableStateOf(null) }
-	var password: String? by remember { mutableStateOf(null) }
+	val montserrat = FontFamily(Font(Res.font.mont))
+	var email by remember { mutableStateOf(Appwrite.currentUser!!.email) }
+	var password by remember { mutableStateOf(Appwrite.currentUser!!.password!!) }
 	
 	Box(
 		modifier = Modifier
+			.fillMaxSize()
 			.padding(16.dp)
 	) {
 		IconButton(
@@ -39,6 +50,28 @@ fun ProfilePage(navigator: Navigator) {
 		Text(
 			text = "Your profile",
 		)
-		
+		OutlinedTextField(
+			value = email,
+			onValueChange = {
+				email = it
+			},
+			leadingIcon = {
+				Icon(
+					painter = painterResource(Res.drawable.ic_email),
+					contentDescription = null,
+				)
+			},
+			placeholder = {
+				Text(
+					text = "Email",
+					style = TextStyle(
+						color = Color.Gray,
+					),
+				)
+			},
+			textStyle = TextStyle(
+				fontFamily = montserrat,
+			),
+		)
 	}
 }
